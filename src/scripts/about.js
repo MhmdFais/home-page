d0c9539d5b23395fbb1e34ab2df0793948da9c50
MhmdFais <117585798+MhmdFais@users.noreply.github.com>
2024-07-29 18:54:07 +0530

    chore: Update about section with profile image, text, and contact links without styling

--- src/scripts/about.js
+++ src/scripts/about.js
@@ -1,4 +1,6 @@
 import ProfileImage from '../assets/images/profile.jpg';
+import Git from '../assets/images/git.png';
+import Link from '../assets/images/lin.svg';
 
 export function aboutSection(){
     createAbout();
@@ -26,8 +28,44 @@ function createImage(){
 
 function createAboutText(){
     const textContainer = document.querySelector(".about-text");
+
+    const header = document.createElement("h1");
+    header.textContent = "About Me";
+    textContainer.appendChild(header);
+
     const text = document.createElement("p");
-    text.textContent = "I am a full stack developer with experience in JavaScript, React, Node.js, and Ruby on Rails. I am passionate about creating user-friendly applications that are both functional and visually appealing. I am currently seeking new opportunities to further develop my skills and contribute to a team.";
+    text.textContent = "I am John, Iam a full stack developer with experience in JavaScript, React, Node.js, and Ruby on Rails. I am passionate about creating user-friendly applications that are both functional and visually appealing. I am currently seeking new opportunities to further develop my skills and contribute to a team.";
     textContainer.appendChild(text);
+
+    const contacts = createContacts();
+    textContainer.appendChild(contacts);
+
     return textContainer;
+}
+
+function createContacts(){
+    const contactsContainer = document.createElement("div")
+    contactsContainer.classList.add("about-contacts-container")
+
+    const git = document.createElement("a");
+    git.href = "";
+    git.classList.add("contact-link");
+    const gitImage = document.createElement("img");
+    gitImage.src = Git;
+    gitImage.alt = "Github";
+    gitImage.classList.add("contact-image");
+    git.appendChild(gitImage);
+    contactsContainer.appendChild(git);
+
+    const linkedin = document.createElement("a");
+    linkedin.href = "";
+    linkedin.classList.add("contact-link");
+    const linkedinImage = document.createElement("img");
+    linkedinImage.src = Link;
+    linkedinImage.alt = "LinkedIn";
+    linkedinImage.classList.add("contact-image");
+    linkedin.appendChild(linkedinImage);
+    contactsContainer.appendChild(linkedin);
+
+    return contactsContainer;
 }
